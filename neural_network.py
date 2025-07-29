9b255531602ba1df6f58e4ec301ae771e2eb3a23
Krish Doshi <krishpdoshi@gmail.com>
2025-07-29 16:00:30 -0700

    adding in dataset

--- neural_network.py
+++ neural_network.py
@@ -2,9 +2,12 @@ import numpy as np
 from activations import sigmoid, sigmoid_deriv, relu, relu_deriv, softmax
 from loss_functions import cross_entropy
 from keras.datasets import mnist
+from keras.utils import to_categorical
 
 
 np.random.seed(0)
+
+learning_rate = 1
 
 class Layer:
     def __init__(self, inputs, neurons, activation=relu):
@@ -32,17 +35,25 @@ class MultilayerPerceptron:
         self.layers.append(Layer(input, neurons, activation))
         
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+train_X = train_X.reshape(-1, 28 * 28)
+test_X = test_X.reshape(-1, 28 * 28)
+
+train_y = to_categorical(train_y, num_classes=10)
+test_y = to_categorical(test_y, num_classes=10)
+
 print('X_train: ' + str(train_X.shape))
 print('Y_train: ' + str(train_y.shape))
 print('X_test:  '  + str(test_X.shape))
 print('Y_test:  '  + str(test_y.shape))
 
+layer1 = Layer(784, 10)
+layer1.forward(train_X[:1])
 
-# layer1 = Layer(784, 10)
-# layer1.forward(X)
+output_layer = Layer(10, 10, softmax)
+output_layer.forward(layer1.output)
 
-# output_layer = Layer(10, 10, softmax)
-# output_layer.forward(layer1.output)
+print(output_layer.output)
 
 # loss = cross_entropy(output_layer.output, y)
 
