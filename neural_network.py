b00049734fb4f73f5bf0703f3e1b167c4fbb45f8
Krish Doshi <krishpdoshi@gmail.com>
2025-07-30 03:10:58 -0700

    completed neural network
    Finished basic neural network. Can combine layers to make a whole multilayer perceptron class for more versatility

--- neural_network.py
+++ neural_network.py
@@ -7,32 +7,63 @@ from keras.utils import to_categorical
 
 np.random.seed(0)
 
-learning_rate = 1
+
+#TODO: Update this class later to make the Multilayer Perceptron more adaptable        
+class MultilayerPerceptron:
+    def __init__(self):
+        self.layers = []
+         
+    def add_layer(self, input, neurons, activation, activation_deriv):
+        self.layers.append(Layer(input, neurons, activation, activation_deriv))
+        
+def batch_generator(X, y, batch_size = 32):
+    n_samples = X.shape[0]
+    for i in range(0, n_samples, batch_size):
+        yield X[i:i+batch_size], y[i:i+batch_size]
+
 
 class Layer:
-    def __init__(self, inputs, neurons, activation=relu):
-        self.weights = 0.1 * np.random.randn(inputs, neurons) #need to be this way for matrix multiplication to work properly
+    def __init__(self, inputs, neurons, activation=relu, activation_deriv=relu_deriv):
+        self.learning_rate = 0.000225
+        self.weights = np.random.randn(inputs, neurons) * np.sqrt(2. / inputs)
+ #need to be this way for matrix multiplication to work properly
         self.biases = np.zeros((1, neurons))
         self.activation = activation
+        self.activation_deriv = activation_deriv
         
     def get_weights(self):
         return self.weights
     
     def forward(self, input):
-        x = np.dot(input, self.weights) + self.biases
-        self.output = self.activation(x)
+        self.input = input
+        self.z = np.dot(input, self.weights) + self.biases
+        self.output = self.activation(self.z)
+        return self.output
         
-    def backwards_output_layer(self, actual, batch_size):
-        self.propogation_error = (self.output - actual) / batch_size
+    def backwards_output_layer(self, y):
+        batch_size = y.shape[0]
+        dz = (self.output - y) / batch_size
+        dW = self.input.T @ dz
+        db = np.sum(dz, axis=0, keepdims=True)
+        
+        self.weights = self.weights - (self.learning_rate * dW)
+        self.biases = self.biases - (self.learning_rate * db)
+        
+        return dz, self.weights
     
+    def backwards_hidden_layers(self, d_next, weights_next):
+        batch_size = self.input.shape[0]
+        
+        d = (d_next @ weights_next.T) * relu_deriv(self.z)
+        dW = (self.input.T @ d) / batch_size
+        db = np.sum(d, axis=0, keepdims=True) / batch_size
+        
+        self.weights = self.weights - (self.learning_rate * dW)
+        self.biases = self.biases - (self.learning_rate * db)
+        
+        return d, self.weights
         
         
-class MultilayerPerceptron:
-    def __init__(self):
-        self.layers = []
-         
-    def add_layer(self, input, neurons, activation):
-        self.layers.append(Layer(input, neurons, activation))
         
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 
@@ -42,22 +73,64 @@ test_X = test_X.reshape(-1, 28 * 28)
 train_y = to_categorical(train_y, num_classes=10)
 test_y = to_categorical(test_y, num_classes=10)
 
-print('X_train: ' + str(train_X.shape))
-print('Y_train: ' + str(train_y.shape))
-print('X_test:  '  + str(test_X.shape))
-print('Y_test:  '  + str(test_y.shape))
+layer1 = Layer(784, 128)
 
-layer1 = Layer(784, 10)
-layer1.forward(train_X[:1])
+output_layer = Layer(128, 10, softmax)
 
-output_layer = Layer(10, 10, softmax)
-output_layer.forward(layer1.output)
+epochs = 100
+for epoch in range(epochs):
+    epoch_loss = 0
+    batch_count = 0
+    
+    if epochs % 10 == 0 and epochs != 0:
+        layer1.learning_rate *= 0.97
+        output_layer.learning_rate *= 0.97
+    
+    for x_batch, y_batch in batch_generator(train_X, train_y, 16):
+        
+        layer1_out = layer1.forward(x_batch)
+        predicted_output = output_layer.forward(layer1_out)
 
-print(output_layer.output)
 
-# loss = cross_entropy(output_layer.output, y)
+        epoch_loss += cross_entropy(predicted_output, y_batch)
+        batch_count += 1
+        
+        d_output, weights_output = output_layer.backwards_output_layer(y_batch)
+        layer1.backwards_hidden_layers(d_output, weights_output)
+    
+    
+    avg_loss = epoch_loss / batch_count
+    print(f'Epoch {epoch + 1}, Avg Loss = {avg_loss:.4f}')
+  
+total_correct = 0  
 
-# output_layer.backwards_output_layer("temp placeholder", output_layer.output.length)
-# print("Loss:", loss)
+for i in range(test_X.shape[0]):
+    layer1_out = layer1.forward(test_X[i:i+1])
+    predicted_output = output_layer.forward(layer1_out)
+    
+    predicted_classes = np.argmax(predicted_output, axis=1)
+    true_classes = np.argmax(test_y[i:i+1], axis=1)
+    
+    if predicted_classes == true_classes:
+        total_correct += 1
+        
+print(f'Accuracy: {total_correct / test_X.shape[0] * 100:.2f}%')
+
+
+    
+    
+
+    
+    
+    
+layer1_out = layer1.forward(test_X[2:3])
+predicted_output = output_layer.forward(layer1_out)
+
+
+
+    
+
+
+
 
     
